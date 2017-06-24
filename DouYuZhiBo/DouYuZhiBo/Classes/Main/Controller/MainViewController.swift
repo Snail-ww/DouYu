5fc1d5db3c782ed14fadd316b82a40ea251020f2
美丽的神话 <Snail@meilidehuadembp.lan>
2017-06-24 10:10:10 +0800

    logo

---
+++ DouYuZhiBo/DouYuZhiBo/Classes/Main/Controller/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  DouYuZhiBo
+//
+//  Created by 美丽的神话 on 17/6/24.
+//  Copyright © 2017年 Snail. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
